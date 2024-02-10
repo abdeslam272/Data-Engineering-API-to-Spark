0f4fef6aad9d5ad332933cd281d5a6eb0c3edabb
Abdeslam EL BENNOURI <71906221+abdeslam272@users.noreply.github.com>
2024-02-10 18:40:53 +0000

    updated extract_api_data.py

--- extract_api_data.py
+++ extract_api_data.py
@@ -1,24 +1,32 @@
-import psycopg2
-from typing import List, Dict, Tuple, Union
+import requests
 import pandas as pd
-import numpy as np
-import datetime
-import time
-import random
 
+def get_random_user_data():
+    url = 'https://randomuser.me/api/'
 
-class API_data_generator():
-  def __init__() -> None:
-    pass
+    # Send a GET request to the API endpoint
+    response = requests.get(url)
 
-  def connect_to_postgres(self, host='192.168.1.193', port = 5432, 
-                            user='postgres', password='1365', database='API_data'):
-    try:
-      conn = psycopg2.connect(host=host, port=port, user=user, password=password, database=database)
-      cur = conn.cursor()
-      print("Connection Succesful")
-      return conn, cur
-    except Exception as e:
-      print(e)
-      raise Exception("Connection Failed")
-      
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response
+        data = response.json()
+
+        # Normalize the JSON data into a pandas DataFrame
+        df_results = pd.json_normalize(data["results"])
+
+        # Selecting desired columns from the existing DataFrame
+        new_df_results = df_results[['gender', 'name.first', 'name.last', 'location.city', 'location.state', 'location.country']].copy()
+
+        # Return the new DataFrame
+        return new_df_results
+    else:
+        print("Failed to retrieve data from the API")
+        return None
+
+# Call the function to get the DataFrame
+random_user_data = get_random_user_data()
+
+# Print the DataFrame (if available)
+if random_user_data is not None:
+    print(random_user_data)
